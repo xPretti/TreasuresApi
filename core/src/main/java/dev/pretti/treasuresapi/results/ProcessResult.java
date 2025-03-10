cc483e2e110598f3ed2d5300109f630c137196c9
Pretti_ <33943962+xPretti@users.noreply.github.com>
2025-03-10 03:44:19 -0300

    feat: Adicionado a lista de tesouros armazenados.

--- core/src/main/java/dev/pretti/treasuresapi/results/ProcessResult.java
+++ core/src/main/java/dev/pretti/treasuresapi/results/ProcessResult.java
@@ -1,11 +1,16 @@
 package dev.pretti.treasuresapi.results;
 
 import dev.pretti.treasuresapi.enums.EnumVanillaDropsType;
+import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class ProcessResult
 {
   private EnumVanillaDropsType removeVanillaDrops;
+  private List<ItemStack>      storage;
 
   @Nullable
   public EnumVanillaDropsType getRemoveVanillaDrops()
@@ -16,5 +21,20 @@ public class ProcessResult
   public void setRemoveVanillaDrops(@Nullable EnumVanillaDropsType removeVanillaDrops)
   {
     this.removeVanillaDrops = removeVanillaDrops;
+  }
+
+  @Nullable
+  public List<ItemStack> getStorege()
+  {
+    return storage;
+  }
+
+  public List<ItemStack> getOrCreateStorege()
+  {
+    if(storage == null)
+      {
+        storage = new ArrayList<>();
+      }
+    return storage;
   }
 }
